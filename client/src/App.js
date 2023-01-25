f0489d30b9a93c3d7bfb7ab87eba65dd04064802
Sumedh Indulkar <cscoder2002@gmail.com>
2023-01-25 18:52:08 +0530

    created form

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,14 @@ import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Card from "./components/Card";
-import { Grid, Container, Box } from "@mui/material";
+import {
+  Grid,
+  Container,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+} from "@mui/material";
 
 function App() {
   const [posts, setPosts] = useState(null);
@@ -20,11 +27,79 @@ function App() {
       <Navbar />
       <Main />
       <Container>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              name="firstName"
+              required
+              fullWidth
+              id="firstName"
+              label="Product Title"
+              placeholder="Rings"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              fullWidth
+              id="lastName"
+              label="Product Description"
+              name="lastName"
+              autoComplete="family-name"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Product Rating"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Product Price"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="email"
+              label="Image URL"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ p: 1, mt: 2 }}
+            >
+              Add Product
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container sx={{ mt: 6 }}>
+        <Grid container spacing={5}>
           {posts &&
             posts.map((item) => {
               return (
-                <Grid key={item.id} item xs={4}>
+                <Grid key={item.id} item xs={4} sx={{ height: "650px" }}>
                   <Card item={item} />
                 </Grid>
               );
